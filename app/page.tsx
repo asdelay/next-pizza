d0e1c7c5ed32ae2b124ce7e0efe807bc2b488a97
Oleksandr Ptukha <souvlaki@Oleksandrs-MacBook-Air.local>
2024-08-02 17:11:01 +0300

    added product card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import {Container, Title, TopBar, Filters } from "@/components/shared";
+import {Container, Title, TopBar, Filters, ProductCard } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
           {/* Goods */}
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              Список товарів
+              <ProductCard id={0} name="Бургер-піца" price={2} imageUrl='https://media.dodostatic.net/image/r:584x584/11EE7D61698827EE9B8DB6D0AEC53410.avif'/>
             </div>
           </div>
         </div>
