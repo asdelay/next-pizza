9fecea4ae3b9d66a394607c102e2be30ca915ae5
Oleksandr Ptukha <souvlaki@Oleksandrs-MacBook-Air.local>
2024-08-09 17:11:52 +0300

    created seed for users

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { prisma } from "./prisma-client"
+import {hashSync} from "bcrypt"
+
+async function up () {
+    await prisma.user.createMany({
+        data: [
+            {
+                fullName: "ali4ka k1rn0sik test", 
+                email: "allka.kirnosik2@gmail.com", 
+                password: hashSync("qweqwewqe", 10), 
+                role: "USER", 
+                verified: new Date()
+            },
+            {
+                fullName: "kitsyunya test", 
+                email: "lysazhopa2@gmail.com", 
+                password: hashSync("admin", 10), 
+                role: "ADMIN", 
+                verified: new Date()
+            },
+        ]
+    })
+}
+
+async function down () {
+    await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+}
+
+async function main () {
+    try {
+        await down()
+        await up()
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+main()
+    .then(async()=>{
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.log(e)
+        await prisma.$disconnect
+        process.exit(1)
+    })
